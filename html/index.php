1255e2362f543c283bbb82023eb4aec1dd6ae659
Mael Bodin <95224514+Maelbo@users.noreply.github.com>
2022-11-16 16:41:25 +0100

    Update index.php

---
+++ html/index.php
@@ -0,0 +1,16 @@
+<form class="pure-form pure-form-stacked">
+    <fieldset>
+        <legend>A Stacked Form</legend>
+        <label for="stacked-email">Nom</label>
+        <input type="Nom" id="stacked-Nom" placeholder="Nom" />
+        <label for="stacked-prenom">Prénom</label>
+        <input type="prenom" id="stacked-prenom" placeholder="Prénom" />
+        <label for="stacked-classe">Classe</label>
+        <select id="stacked-classe">
+            <option>R&T</option>
+            <option>MP</option>
+            <option>TC</option>
+        </select>
+        <button type="submit" class="pure-button pure-button-primary">Sign in</button>
+    </fieldset>
+</form>
