268daf9ecfc50b0230d455e72013a7b72dbeedfe
Armand <95224587+Armand-Cloug@users.noreply.github.com>
2022-11-16 22:53:37 +0100

    Add files via upload

--- helper/connection.php
+++ helper/connection.php
@@ -1,3 +1,7 @@
-<?php
-$pdo = new PDO('mysql:host=127.0.0.1;dbname=Savant', 'Savant', 'gtrnet');
+<?php $pdo = new PDO('mysql:host=127.0.0.1;dbname=Savant', 'savant', 'gtrnet'); 
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  
 ?>
--- helper/header.php
+++ helper/header.php
@@ -1,10 +1,12 @@
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimal-ui">
-    <title>COVID19 site</title>
-    <link rel="stylesheet" href="assets/style.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Quizz Rendez vous Savant </title>
+    <link rel="stylesheet" href="https://unpkg.com/purecss@2.1.0/build/pure-min.css" integrity="sha384-yHIFVG6ClnONEA5yB5DJXfW2/KC173DIQrYoZMEtBvGzmf0PKiGyNEqe9N6BNDBH" crossorigin="anonymous">
+    <link rel="stylesheet" href="C:\Apache24\htdocs\assets\style.css"/>
+    <script src="C:\Apache24\htdocs\assets\script.js"></script>
+    <link rel="icon" href="icon.png">
 </head>
